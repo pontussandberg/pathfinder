c2d790f49c3914d1245164dde75d6bc441f07301
pontussandberg <sandbergpontus97@gmail.com>
2020-01-17 19:54:39 +0100

    v2.0

--- README.md
+++
@@ -1,5 +0,0 @@
-<h1>Pathfinder</h1>
-
-<p>This app will find a way to get from point A - B in a grid</p> 
-<br>
-Demo <a target="_blank" href="https://pathfinder-app.netlify.com/">page</a>
--- app.js
+++ app.js
@@ -1,282 +1,330 @@
-let displayStats = document.getElementById('stats');
+// Classes
+class Square {
+    constructor(col, row, context) {
+        this.col = col;
+        this.row = row;
+        this.x = this.col * 20;
+        this.y = this.row * 20;
+        this.width = 20;
+        this.height = 20;
+        this.ctx = context;
+    }
+    draw(color) {
+        if (color) this.ctx.fillStyle = color;
+        else this.ctx.fillStyle = "#fff";
+        this.ctx.beginPath();
+        this.ctx.fillRect(this.x, this.y, 19, 19);
+        this.ctx.stroke();
+    }
+
+    log() {
+        console.log(x);
+    }
+}
+
+class Game {
+    constructor() {
+        this.board = this.getBoard();
+        this.isRunning = false;
+        this.isRunningBack = false;
+        this.options = [];
+        this.wallsArr = [];
+        this.bestPath = [];
+        this.initalPos;
+        this.finishPos;
+        this.currentPos;
+        this.interval;
+    }
+
+    handleStart() {
+        this.reset(); // keeps the walls
+
+        let startValue = document.getElementById('start-input').value;
+        let endValue = document.getElementById('end-input').value;
+
+        this.runInit(startValue || 0, endValue || 624);
+    }
+
+    runInit = (startPos, endPos) => {
+        this.currentPos = this.board[startPos];
+        this.initalPos = this.currentPos;
+        this.finishPos = this.board[endPos];
+        this.board[endPos].endPos = true;
+        this.currentPos.draw("yellow");
+
+        this.isRunning = true;
+        this.interval = setInterval(this.runLoop, 30)
+    }
+
+
+
+    runLoop = () => {
+        if (this.currentPos !== this.finishPos && this.isRunning) {
+            this.makeCellVisited();
+            this.checkOptions(this.board);
+            this.filterDuplicates();
+            this.filterVisited();
+            this.move();
+            this.paintBoard();
+        }
+        else {
+            clearInterval(this.interval);
+            this.runBackInit();
+        }
+    }
+
+    runBackInit = () => {
+        let newEndPos = this.board.indexOf(this.initalPos);  // setting new end pos to the initial pos
+        let oldEndPos = this.board.indexOf(this.finishPos); // setting current pos to inital finish pos 
+        this.board[newEndPos].endPos = true;
+        this.board[oldEndPos].endPos = false;
+        this.finishPos = this.board[newEndPos];
+
+        this.isRunningBack = true;
+        this.options = [];
+
+        this.interval = setInterval(this.runBackLoop, 30);
+    }
+
+    runBackLoop = () => {
+        if (this.currentPos !== this.finishPos && this.isRunning) {
+            this.makeCellPathed();
+            this.checkOptions(this.visited);
+            this.filterBestPath();
+            this.filterDuplicates();
+            this.move();
+            this.paintBoard();
+        }
+        else {
+            clearInterval(this.interval);
+            this.finishPos.draw("yellow");
+            this.initalPos.draw("yellow");
+            this.isRunning = false;
+            this.isRunningBack = false;
+            console.log("done");
+        }
+    }
+
+    checkOptions(arr) {
+        let cp = this.currentPos;
+        for (let i = 0; i < arr.length; i++) {
+            let isValidOption = false;
+
+
+            // same row, +1 col
+            if (cp.col + 1 === arr[i].col && cp.row === arr[i].row)
+                isValidOption = true;
+
+            // same col, +1 row
+            if (cp.row + 1 === arr[i].row && cp.col === arr[i].col)
+                isValidOption = true;
+
+            // same row, +1 col
+            if (cp.col - 1 === arr[i].col && cp.row === arr[i].row)
+                isValidOption = true;
+
+            // same col, +1 row
+            if (cp.row - 1 === arr[i].row && cp.col === arr[i].col)
+                isValidOption = true;
+
+            if (arr[i].wall)
+                isValidOption = false;
+
+            if (arr[i].bestPath) {
+                isValidOption = false;
+            }
+            // filtering out visited cells for the first part of program
+            if (!this.isRunningBack) {
+                if (arr[i].visited)
+                    isValidOption = false;
+            }
+
+
+            if (isValidOption) {
+                this.options.push(arr[i]);
+            }
+        }
+    }
+
+
+
+
+    move() {
+        let shortestDist;
+        let bestOption;
+
+        if (this.options) {
+            for (let i = 0; i < this.options.length; i++) {
+
+                // Can't do pythagoras on the neighbor cell. Need explicit check for finish.
+                if (this.options[i].endPos) {
+                    bestOption = this.options[i];
+                    break;
+                }
+
+                // Pythagoras theroem
+                let distance = Math.sqrt(Math.pow((this.options[i].x - this.finishPos.x), 2) + Math.pow((this.options[i].y - this.finishPos.y), 2));
+                if (distance < shortestDist || !shortestDist) {
+                    shortestDist = distance;
+                    bestOption = this.options[i];
+                }
+            }
+            this.currentPos = bestOption;
+        }
+    }
+
+
+    makeCellVisited() {
+        let index = this.board.indexOf(this.currentPos);
+        this.board[index].visited = true;
+        this.visited.push(this.board[index]);
+    }
+
+    makeCellPathed() {
+
+        let index = this.board.indexOf(this.currentPos);
+        this.bestPath.push(this.currentPos);
+        this.board[index].bestPath = true;
+
+    }
+
+    filterVisited() {
+        if (this.options) {
+            for (let i = 0; i < this.options.length; i++) {
+                if (this.options[i].visited) {
+                    this.options.splice(i, 1);
+                }
+            }
+        }
+    }
+
+    filterBestPath() {
+        for (let i = 0; i < this.options.length; i++) {
+            for (let j = 0; j < this.bestPath.length; j++) {
+                if (this.options[i].col === this.bestPath[j].col && this.options[i].row === this.bestPath[j].row) {
+                    this.options.splice(i, 1);
+                }
+            }
+        }
+    }
+
+    filterDuplicates() {
+        if (this.options)
+            this.options.filter((a, b) => this.options.indexOf(a) === b)
+    }
+
+    handleClick(e) {
+        let clickedX = Math.floor(e.offsetX / 20) * 20;
+        let clickedY = Math.floor(e.offsetY / 20) * 20;
+        for (let i = 0; i < this.board.length; i++) {
+            if (this.board[i].y === clickedY && this.board[i].x === clickedX) {
+                if (this.board[i].wall) {
+                    this.board[i].wall = false;
+                    this.board[i].draw('#fff');
+                }
+                else {
+                    this.board[i].draw('#111');
+                }
+            }
+        }
+    }
+
+    reset() {
+        // resetting board except for walls
+        let newBoard = this.getBoard();
+        for (let i = 0; i < newBoard.length; i++) {
+            for (let j = 0; j < this.board.length; j++) {
+                if ((this.board[i].wall) && (newBoard[i].col === this.board[j].col && newBoard[i].row === this.board[j].row)) {
+                    newBoard[i].wall = true;
+                }
+            }
+        }
+        // setting board with same walls but resetting everything else
+        this.board = newBoard;
+        this.options = [];
+        this.currentPos = null;
+        this.isRunning = false;
+        this.isRunningBack = false;
+        this.options = [];
+        this.visited = [];
+        this.bestPath = [];
+        this.initalPos = null;
+        this.finishPos = null;
+        this.paintBoard();
+    }
+
+    getDefaultWalls() {
+        let defaultWalls = [617, 592, 567, 542, 517, 492, 467, 417,
+            442, 392, 367, 342, 343, 344, 345, 346, 347, 348, 623,
+            598, 573, 548, 523, 492, 473, 472, 471, 470, 469, 474,
+            323, 298, 273, 248, 223, 198, 173, 148, 123, 98, 73,
+            48, 47, 46, 41, 40, 39, 38, 37, 35, 34, 33, 32, 31,
+            30, 29, 28, 27, 26, 25, 66, 91, 116, 191, 216, 241,
+            266, 291, 316, 341, 240, 239, 238, 237, 236, 235,
+            234, 233, 208, 183, 158, 133, 108, 83, 58, 134,
+            160, 186, 212, 211, 210, 185, 209, 184, 159,
+            135, 161, 187, 213, 109];
+
+        for (let i = 0; i < this.board.length; i++) {
+            for (let j = 0; j < defaultWalls.length; j++) {
+                if (defaultWalls[j] === i) {
+                    this.board[i].wall = true;
+                }
+            }
+        }
+        this.paintBoard();
+    }
+
+    getBoard() {
+        let boardArr = [];
+        for (let row = 0; row < 25; row++) {
+            for (let col = 0; col < 25; col++) {
+                boardArr.push(new Square(col, row, ctx))
+            }
+        }
+        return boardArr;
+    }
+
+    paintBoard() {
+        for (let i = 0; i < this.board.length; i++) {
+            if (this.board[i].wall) this.board[i].draw("#111");             // wall
+            else if (this.board[i].bestPath) this.board[i].draw("#007bff"); // visited
+            else if (this.board[i].endPos) this.board[i].draw("#d95350");   // best path
+            else if (this.board[i].visited) this.board[i].draw("#27a744");  // finish
+            else this.board[i].draw("#fff");                                // empty
+        }
+    }
+
+    drawArr(arr, color) {
+        for (let i = 0; i < arr.length; i++) {
+            arr[i].draw(color);
+        }
+    }
+
+}
+
+// init
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext("2d");
+canvas.addEventListener('mousedown', handleClick);
 
 // Canvas must be a square
 canvas.height = 500;
 canvas.width = 500;
 
-// Classes
-class Square {
-	constructor(col, row) {
-		this.col = col;
-		this.row = row;
-		this.x = this.col * 10;
-		this.y = this.row * 10;
-
-		this.width = 10;
-		this.height = 10;
-	}
-	draw(color) {
-		if (color) ctx.fillStyle = color;
-		else ctx.fillStyle = "#fff";
-		ctx.beginPath();
-		ctx.fillRect(this.x, this.y, 9, 9);
-		ctx.stroke();
-	}
-}
-
-canvas.addEventListener('mousedown', handleClick);
-
-
-
-
-
-
-// gets arr with square obj which represents the board.
-let board = getBoard();
-
-let visited = [];
-let options = [];
-let walls = [];
-let currentPos;
-let interval;
-
-drawAll();
-
-
-
-
-function handleStart() {
-	let startValue = document.getElementById('start-input').value;
-	let endValue = document.getElementById('end-input').value;
-	let intervalValue = document.getElementById('interval-input').value;
-
-	initializer(startValue || 0, endValue || 2499, intervalValue || 100);
-}
-
-
-
-
-
-// Kicking off find();
-function initializer(startPos, endPos, intervalTimer) {
-	reset();
-	currentPos = board[startPos];
-	finishPos = board[endPos];
-	finishPos.draw("#f48024");
-	visited.push(currentPos);
-	currentPos.draw("yellow");
-
-	interval = setInterval(find, intervalTimer);
-}
-
-
-
-
-
-// Main function. The loop.
-function find() {
-
-	if (currentPos !== finishPos) {
-
-		checkForOptions();
-		removeVisited();
-		currentPos = chooseBestOption();
-		visited.push(currentPos);
-		drawVisited();
-	}
-	else {
-		clearInterval(interval);
-	}
-}
-
-
-
-
-// Drawing the visited cells on Canvas.
-function drawVisited() {
-	for (let i = 0; i < visited.length; i++) {
-		visited[i].draw('#27a744');
-	}
-}
-
-
-
-
-
-// After checking for options, removing all options
-// that are already visited. 
-// Also removes "walls" from options.
-function removeVisited() {
-	for (let i = 0; i < options.length; i++) {
-
-		for (let j = 0; j < visited.length; j++) {
-			if (options[i] === visited[j]) {
-				options.splice(i, 1);
-			}
-		}
-	}
-}
-
-
-
-
-
-
-
-// Calculate the option with the closest path
-// to endPoint and returns that value.
-// Also removes that option from options array.
-function chooseBestOption() {
-	if (options) {
-		let closestCell = 10000000000;
-		let bestCell;
-		for (let i = 0; i < options.length; i++) {
-
-			let value = Math.sqrt(Math.pow((options[i].x - finishPos.x), 2) + Math.pow((options[i].y - finishPos.y), 2));
-
-			if (value < closestCell) {
-				closestCell = value;
-				bestCell = options[i];
-			}
-
-
-		}
-		let index = options.indexOf(bestCell);
-		options.splice(index, 1);
-		return bestCell;
-	}
-	else alert('no options');
-}
-
-
-
-
-
-
-// Finds "neighbor" cells and lists them to options array.
-function checkForOptions() {
-	if (currentPos) {
-		for (let i = 0; i < board.length; i++) {
-
-			let isRow = false;
-			let isCol = false;
-
-			// straight?
-			if (currentPos.col === board[i].col || currentPos.col + 1 === board[i].col || currentPos.col - 1 === board[i].col) {
-				isCol = true;
-			}
-
-			if (board[i].row === currentPos.row || board[i].row - 1 === currentPos.row || board[i].row + 1 === currentPos.row) {
-				isRow = true;
-			}
-
-			// diagonal?
-			if (currentPos.row + 1 === board[i].row && currentPos.col + 1 === board[i].col)
-				continue;
-
-
-			if (currentPos.row - 1 === board[i].row && currentPos.col - 1 === board[i].col)
-				continue;
-
-
-			if (currentPos.row - 1 === board[i].row && currentPos.col + 1 === board[i].col)
-				continue;
-
-
-			if (currentPos.row + 1 === board[i].row && currentPos.col - 1 === board[i].col)
-				continue;
-
-
-			// wall?
-			if (board[i].wall === true)
-				continue;
-
-
-			// same?
-			if (currentPos === board[i])
-				continue;
-
-			// valid option?
-			if (isRow && isCol) {
-				options.push(board[i]);
-				console.log(board[i], "the one getting pushed");
-			}
-		}
-	}
-
-
-	else {
-		clearInterval(interval);
-		alert('I am stuck');
-	}
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-function reset() {
-	clearInterval(interval);
-	visited = [];
-	options = [];
-	currentPos;
-	interval;
-	drawAll();
-}
+let game = new Game();
+game.paintBoard();
+game.getDefaultWalls();
 
 function handleReset() {
-	board = getBoard();
-	reset();
+    game = new Game();
+    game.paintBoard();
 }
-
-
-function getBoard() {
-	let boardArr = [];
-	for (let row = 0; row < 50; row++) {
-		for (let col = 0; col < 50; col++) {
-			boardArr.push(new Square(col, row))
-		}
-	}
-	return boardArr;
-}
-
-
-
-function drawAll() {
-	for (let i = 0; i < board.length; i++) {
-		if (board[i].wall === true) board[i].draw("#111");
-		else board[i].draw();
-
-	}
-
-}
-
 
 function handleClick(e) {
-	let clickedX = Math.floor(e.offsetX / 10) * 10;
-	let clickedY = Math.floor(e.offsetY / 10) * 10;
-	for (let i = 0; i < board.length; i++) {
-		if (board[i].y === clickedY && board[i].x === clickedX) {
-			if (board[i].wall) {
-				board[i].wall = false;
-				board[i].draw('#fff');
-			}
-			else {
-				board[i].wall = true;
-				board[i].draw('#111');
-			}
-		}
-	}
+    game.handleClick(e);
 }
 
+function handleStart() {
+    game.handleStart();
+}
